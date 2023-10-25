15d279a0a849ebe60a64d81cc1f196a551a903f3
punithgeepalyam <punith.geepalyam@ensarsolutions.com>
2023-10-25 23:53:24 +0530

    Add project files.

---
+++ StartUp.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestRDCache.Data;
+using System.Configuration;
+using Microsoft.Extensions.Configuration;
+
+
+namespace TestRDCache
+{
+    public class StartUp
+    {
+        //public void ConfigureServices(IServiceCollection services)
+        //{
+        //    services.AddControllersWithViews();
+        //    var configuration = new ConfigurationBuilder()
+        //       .SetBasePath(Directory.GetCurrentDirectory())
+        //       .AddJsonFile("appsettings.json")
+        //       .Build();
+        //    services.AddSingleton<IConfiguration>(configuration);
+
+
+        //    services.AddDbContext<AdventureWorksDbContext>(options =>
+        //        options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+        //}
+
+        //Only When using Redis Cache
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddControllersWithViews();
+            var configuration = new ConfigurationBuilder()
+               .SetBasePath(Directory.GetCurrentDirectory())
+               .AddJsonFile("appsettings.json")
+               .Build();
+            services.AddSingleton<IConfiguration>(configuration);
+
+
+            services.AddDbContext<AdventureWorksDbContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddStackExchangeRedisCache(options =>
+            {
+                options.Configuration = configuration["RedisCacheServerUrl"];
+            });
+        }
+
+
+    }
+}
